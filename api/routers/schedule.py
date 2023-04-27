3a6d48372898fb075215ef85f40139ff0094af97
KienLT <kienlt@otani-trading.com>
2023-04-27 10:30:37 +0700

    API

--- api/routers/schedule.py
+++ api/routers/schedule.py
@@ -7,11 +7,11 @@ from pydantic import BaseModel
 
 router = APIRouter()
 class Schedules(BaseModel):
-    id : int
-    date : date
-    time : time
-    fieldfee : int
-    participants : int[]
+    id: int
+    date: date
+    time: time
+    fieldfee: int
+    participants: []
 
 
 @router.get("/api/schedule")
@@ -26,3 +26,30 @@ def get_schedule_by_id(id: int):
     """Get schedule by id"""
     schedule = model_to_dict(Schedule.get(Schedule.id == id))
     return schedule
+
+
+@router.post("/api/schedule")
+def create_schedule(payload_: Schedule):
+    """Create new schedule"""
+    payload = payload_.dict()
+    Schedule.create(**payload)
+
+
+@router.patch("/api/schedule/{id}")
+def update_schedule(id: int, payload_: Schedules):
+    """Update schedule"""
+    payload = payload_.dict()
+    schedule = (
+        Schedule.update(**payload)
+        .where(Schedule.id == id)
+        .execute()
+    )
+    # number of changed rows
+    return schedule
+
+
+@router.delete("/api/schedule/{id}")
+def delete_schedule(id: int):
+    """Delete schedule"""
+    schedule = Schedule.get_by_id(id)
+    schedule.delete_instance()
